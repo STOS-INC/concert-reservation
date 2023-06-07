89b38f63d0ed07d3d71f555b8b7fe5c5c6852986
shindh12 <shindh159@gmail.com>
2023-06-07 19:52:42 +0900

    first commit

---
+++ src/main/java/com/stos/concert/ConcertReservationApplication.java
@@ -0,0 +1,13 @@
+package com.stos.concert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertReservationApplication.class, args);
+	}
+
+}
