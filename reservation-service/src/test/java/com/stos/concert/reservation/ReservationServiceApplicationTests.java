d8bd1d99681ac3f9b4047e61457d4681d6afa06b
shindh12 <shindh159@gmail.com>
2023-06-26 22:01:42 +0900

    add reservation service

---
+++ reservation-service/src/test/java/com/stos/concert/reservation/ReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stos.concert.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
