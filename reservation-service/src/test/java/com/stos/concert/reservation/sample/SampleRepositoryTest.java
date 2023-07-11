f9a1566281db668730c028ab2aff4f8d4063287e
biiin <jakajaka51@naver.com>
2023-07-11 23:24:56 +0900

    refactor : unused import module 제거

--- reservation-service/src/test/java/com/stos/concert/reservation/sample/SampleRepositoryTest.java
+++ reservation-service/src/test/java/com/stos/concert/reservation/sample/SampleRepositoryTest.java
@@ -1,28 +1,11 @@
 package com.stos.concert.reservation.sample;
 
-import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
-
-import com.stos.concert.reservation.TestReservationServiceApplication;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.time.Duration;
-import javax.sql.DataSource;
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @DataJpaTest
 @ActiveProfiles("test")
