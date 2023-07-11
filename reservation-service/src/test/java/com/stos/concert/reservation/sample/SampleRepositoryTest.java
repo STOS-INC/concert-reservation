9cbb9d7c20a05f59af3ea6bef70b5b331636eb88
biiin <jakajaka51@naver.com>
2023-07-11 23:26:15 +0900

    test : 프로젝트 공통 테스트 라이브러리(AssertJ)로 변경

--- reservation-service/src/test/java/com/stos/concert/reservation/sample/SampleRepositoryTest.java
+++ reservation-service/src/test/java/com/stos/concert/reservation/sample/SampleRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.stos.concert.reservation.sample;
 
-import org.junit.jupiter.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -22,6 +23,6 @@ public class SampleRepositoryTest {
 
     SampleEntity res = sampleRepository.findById(1L).orElseThrow();
 
-    Assertions.assertEquals("TEST 1", res.getName());
+    assertThat("TEST 1").isEqualTo(res.getName());
   }
 }
