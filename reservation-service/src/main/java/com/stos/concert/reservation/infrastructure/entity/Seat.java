09a5744b59039d4e1fa67ae3d1aac1c53d3b65d0
biiin <jakajaka51@naver.com>
2023-07-12 19:41:56 +0900

    refactor : mysql 예약어(row)와 겹쳐 이름 변경

--- reservation-service/src/main/java/com/stos/concert/reservation/infrastructure/entity/Seat.java
+++ reservation-service/src/main/java/com/stos/concert/reservation/infrastructure/entity/Seat.java
@@ -1,11 +1,19 @@
 package com.stos.concert.reservation.infrastructure.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Seat {
   private String type;
   private String section;
-  private String row;
+  private String rowNumber;
   private String number;
 }
