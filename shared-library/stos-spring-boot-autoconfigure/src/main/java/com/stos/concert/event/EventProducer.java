ecd6719a0a0397f5611ff33f4ccffc9929efb960
biiin <jakajaka51@naver.com>
2023-11-09 00:24:44 +0900

    feat : producer config key 상수 사용

--- shared-library/stos-spring-boot-autoconfigure/src/main/java/com/stos/concert/event/EventProducer.java
+++ shared-library/stos-spring-boot-autoconfigure/src/main/java/com/stos/concert/event/EventProducer.java
@@ -4,6 +4,7 @@ import com.stos.concert.shared.Event.DomainEvent;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -25,9 +26,9 @@ public class EventProducer {
 
   private ProducerFactory<String, Message<DomainEvent>> producerFactory() {
     Map<String, Object> config = new HashMap<>();
-    config.put("bootstrap.servers", "localhost:9092");
-    config.put("key.serializer", StringSerializer.class);
-    config.put("value.serializer", JsonSerializer.class);
+    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
     return new DefaultKafkaProducerFactory<>(config);
   }
 
